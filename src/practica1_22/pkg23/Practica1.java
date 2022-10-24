1a45f9e204c5a7cf7a846c128468132d783b17a1
Ramso <Ramso@DESKTOP-F436ANM>
2022-10-24 15:23:51 +0200

    1. Practica 1 inicio

---
+++ src/practica1_22/pkg23/Practica1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_22.pkg23;
+
+/**
+ *
+ * @author Ramso
+ */
+public class Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* PAT github */
+        /* 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 */
+    }
+    
+}
