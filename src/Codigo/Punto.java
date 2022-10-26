5a86c8f02867b6e66ed35a8242cea5481e2a6a67
Gabo-Dev <jonathangope10@gmail.com>
2022-10-26 12:30:44 +0200

    Update Punto

--- src/Codigo/Punto.java
+++ src/Codigo/Punto.java
@@ -10,5 +10,21 @@ package Codigo;
  * @author Ramso
  */
 public class Punto {
-    
+    private int x;
+    private int y;
+    //Getters
+    public void setX(int x){
+        this.x=x;
+    }
+    public void setY(int y){
+        this.y=y;
+    }
+    //Setters
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+
 }
